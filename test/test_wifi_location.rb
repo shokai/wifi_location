ba48b21536c40f212a31da0d69e11defe73c5b0c
Sho Hashimoto <hashimoto@shokai.org>
2012-08-03 11:39:03 +0900

    add tests

--- test/test_wifi_location.rb
+++ test/test_wifi_location.rb
@@ -1,11 +1,27 @@
 require File.dirname(__FILE__) + '/test_helper.rb'
 
 class TestWiFiLocation < Test::Unit::TestCase
-
   def setup
+    @location = WiFiLocation.location
   end
-  
-  def test_truth
-    assert true
+
+  def test_location
+    assert @location.class == Hash
+  end
+
+  def test_latitude
+    assert @location['latitude'].class == Float
+  end
+
+  def test_longitude
+    assert @location['longitude'].class == Float
+  end
+
+  def test_address
+    assert @location['address'].class == Hash
+  end
+
+  def test_accuracy
+    assert @location['accuracy'].class == Float
   end
 end
