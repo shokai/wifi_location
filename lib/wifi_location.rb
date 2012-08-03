4ba1725997decbcdcadee516ea8e479487fdc808
Sho Hashimoto <hashimoto@shokai.org>
2012-08-03 11:28:17 +0900

    WiFiLocation.location

--- lib/wifi_location.rb
+++ lib/wifi_location.rb
@@ -1,6 +1,44 @@
-$:.unshift(File.dirname(__FILE__)) unless
-  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
+require 'rubygems'
+require 'net/http'
+require 'uri'
+require 'json'
 
-module WifiLocation
+module WiFiLocation
   VERSION = '0.0.1'
+
+  def self.wifi_towers
+    case RUBY_PLATFORM
+    when /darwin/
+      lines = `/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport -s`.split(/[\r\n]/)
+      lines.shift
+      lines.map{|i|
+        a = i.scan(/[^\s]+/)
+        {:bssid => a[1], :signal => a[2].to_i}
+      }
+    when /linux/
+      lines = `iwlist wlan0 scan`.split(/[\r\n]/)
+      addrs = []
+      lines.each do |line|
+        addrs.push({:bssid => line.split(/\s+/).last, :signal => 8}) if line =~ /Address: /
+        addrs.last[:signal] = line.scan(/Signal level=(\-?\d+)/)[0][0].to_i rescue next if line =~ /Signal level=/
+      end
+      addrs
+    end
+  end
+
+  def self.location(wifi_towers=self.wifi_towers)
+    uri = URI.parse 'http://www.google.com/loc/json'
+    wifi_towers = self.wifi_towers unless wifi_towers
+    query = {
+      :version => '1.1.0',
+      :host => 'maps.google.com',
+      :request_address => true,
+      :address_language => ENV['LANG'] ? ENV['LANG'].scan(/([^\.]+)/)[0][0] : 'en_US',
+      :wifi_towers => wifi_towers.map{|addr| {:mac_address => addr[:bssid], :signal_strength => addr[:signal], :age => 0} }
+    }.to_json
+    res = Net::HTTP.start(uri.host, uri.port).request(Net::HTTP::Post.new(uri.request_uri), query)
+    raise "Response Error (#{res.code})" unless res.code.to_i == 200
+    JSON.parse(res.body)['location']
+  end
+
 end
--- test/test_wifi_location.rb
+++ test/test_wifi_location.rb
@@ -1,6 +1,6 @@
 require File.dirname(__FILE__) + '/test_helper.rb'
 
-class TestWifiLocation < Test::Unit::TestCase
+class TestWiFiLocation < Test::Unit::TestCase
 
   def setup
   end
